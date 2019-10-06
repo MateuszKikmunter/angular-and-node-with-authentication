231c4cf94147fbd672a25160d26319e7f388f4ef
m.kikmunter <M.Kikmunter@gmail.com>
2019-10-06 22:01:00 +0100

    restricted CORS rules

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,25 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+    "http://localhost:4200"
+];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = "The CORS policy for this resource does not allow access from the specified origin.";
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 app.get("/api/messages", (req, res) => {
-    
+
     const messages = [
         { text: "hello", owner: "Matt" },
         { text: "duck!! duck!!", owner: "Ducky" },
