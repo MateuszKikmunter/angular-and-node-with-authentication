03df64d28aa25bbb37c318aa6b34e429b6421c51
m.kikmunter <M.Kikmunter@gmail.com>
2019-10-06 21:44:43 +0100

    fixed unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularNodeAuthentication'`, () => {
+  it(`should have as title 'Angular-Node-Authentication'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('AngularNodeAuthentication');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AngularNodeAuthentication app is running!');
+    expect(app.title).toEqual('Angular-Node-Authentication');
   });
 });
--- src/app/message/message/message.component.spec.ts
+++ src/app/message/message/message.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MaterialModule } from './../../material/material.module';
+
 import { MessageComponent } from './message.component';
+import { MessageService } from 'src/app/core/services/message.service';
+import { Observable, of } from 'rxjs';
 
 describe('MessageComponent', () => {
   let component: MessageComponent;
@@ -8,14 +12,20 @@ describe('MessageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MessageComponent ]
+      imports: [
+        MaterialModule
+      ],
+      declarations: [
+        MessageComponent
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MessageComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
