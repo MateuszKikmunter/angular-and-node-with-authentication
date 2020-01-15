5cb4e8a9ef642706caf30b3ffe760f472a1b98ee
m.kikmunter <M.Kikmunter@gmail.com>
2020-01-15 21:57:44 +0000

    reformat code

--- src/app/auth/log-in/log-in.component.ts
+++ src/app/auth/log-in/log-in.component.ts
@@ -1,35 +1,47 @@
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
 
-import { AuthService } from './../../core/services/auth.service';
+import { AuthService } from "./../../core/services/auth.service";
 
 @Component({
-  selector: 'app-log-in',
-  templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.scss']
+  selector: "app-log-in",
+  templateUrl: "./log-in.component.html",
+  styleUrls: ["./log-in.component.scss"]
 })
 export class LogInComponent implements OnInit {
-
   public logInForm: FormGroup;
 
   constructor(
     private authService: AuthService,
     private formBuilder: FormBuilder
-    ) { }
+  ) {}
 
   ngOnInit() {
     this.buildForm();
   }
 
   public onSubmit(): void {
-    
   }
 
   private buildForm(): void {
     this.logInForm = this.formBuilder.group({
-      email: [ null, [ Validators.required, Validators.maxLength(255), Validators.minLength(3), Validators.email ] ],
-      password: [ null, [ Validators.required, Validators.maxLength(255), Validators.minLength(3) ] ],
+      email: [
+        null,
+        [
+          Validators.required,
+          Validators.maxLength(255),
+          Validators.minLength(3),
+          Validators.email
+        ]
+      ],
+      password: [
+        null,
+        [
+          Validators.required,
+          Validators.maxLength(255),
+          Validators.minLength(3)
+        ]
+      ]
     });
   }
-
 }
