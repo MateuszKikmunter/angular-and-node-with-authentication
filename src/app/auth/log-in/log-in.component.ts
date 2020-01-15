49941cf194b506b58850ba0f6d5526abdee3a156
m.kikmunter <M.Kikmunter@gmail.com>
2020-01-15 21:49:43 +0000

    added template for login component

--- src/app/auth/log-in/log-in.component.ts
+++ src/app/auth/log-in/log-in.component.ts
@@ -1,4 +1,7 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from './../../core/services/auth.service';
 
 @Component({
   selector: 'app-log-in',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  public logInForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+    ) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  public onSubmit(): void {
+    
+  }
+
+  private buildForm(): void {
+    this.logInForm = this.formBuilder.group({
+      email: [ null, [ Validators.required, Validators.maxLength(255), Validators.minLength(3), Validators.email ] ],
+      password: [ null, [ Validators.required, Validators.maxLength(255), Validators.minLength(3) ] ],
+    });
   }
 
 }
