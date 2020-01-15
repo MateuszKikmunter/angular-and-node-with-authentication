4c7d98dfa3dc44e14dd3b3f07a86015ba6994835
m.kikmunter <M.Kikmunter@gmail.com>
2020-01-15 21:40:22 +0000

    added route for login component

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,17 @@
-import { RegisterComponent } from "./register/register.component";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+
+import { LogInComponent } from './log-in/log-in.component';
+import { RegisterComponent } from "./register/register.component";
 
 const authRoutes: Routes = [
   {
     path: "register",
     component: RegisterComponent
+  },
+  {
+    path: "login",
+    component: LogInComponent
   }
 ];
 
