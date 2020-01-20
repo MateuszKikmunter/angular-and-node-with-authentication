774bdd74a9ef561de24a8ec338d35fd95d814684
m.kikmunter <M.Kikmunter@gmail.com>
2020-01-20 12:22:54 +0000

    removed logging statements

--- src/app/core/interceptors/auth-interceptor.service.ts
+++ src/app/core/interceptors/auth-interceptor.service.ts
@@ -8,7 +8,6 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor() { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log("in auth interceptor");
     const token: string = localStorage.getItem("token");
     if (token) {
       request = request.clone({ headers: request.headers.set('Authorization', `Bearer ${token}`) });
